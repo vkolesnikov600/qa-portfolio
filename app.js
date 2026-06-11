5c4b82c9dcc17ce818a1b72a13e6e7943ce9b54b
Vladislav Kolesnikov <vkolesnikov600@gmail.com>
2026-06-11 13:37:13 +0600

    Prepare site for Netlify deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const mainRoutes = require('./routes/mainRoutes');
 const logger = require('./middleware/logger');
